a70260e7c9383e922627fc10f1ef32003b7393ad
Caroline <Caroline@CAROLINE>
2022-10-31 11:21:27 +0100

    Add project files.

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
