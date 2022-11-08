407f626406deb650c0603521de5c2734182bc0ce
Caroline <Caroline@CAROLINE>
2022-11-08 10:56:09 +0100

    mvc_data first assignment

---
+++ MVC/ViewModels/PeopleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MVC.Models;
+
+namespace MVC.ViewModels
+{
+    public class PeopleViewModel : CreatePersonViewModel
+    {
+        public List<Person> PeopleList { get; set; } = new List<Person>();
+        public string Search { get; set; } 
+        public bool CaseSensitive { get; set; }
+
+   
+
+
+    }
+}
